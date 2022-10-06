65b2c5902fcf21f3f57c4adfc61c1b079479b8a8
edurosetech <eduardo.rose92@gmail.com>
2022-10-06 16:22:37 -0300

    section 4 - final

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import App from "@/App.vue";
+import { createApp } from "vue";
+import "./assets/tailwind.css";
+
+createApp(App).mount("#app");
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./public/**/*.html", "./src.**.*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+
+  plugins: [],
+};
