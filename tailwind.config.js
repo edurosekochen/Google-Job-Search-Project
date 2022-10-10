ae78680545974d7c20dd8bc95067d755c0738437
Eduardo <eduardorosenberg92@gmail.com>
2022-10-10 15:29:53 -0300

    section-5-complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,40 @@
+// const defaultTheme = require("tailwindcss/defaultTheme");
+
+// module.exports = {
+//   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+//   content: ["./public/**/*.html", "./src.**.*.{vue,js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+//       },
+//       colors: {
+//         "brand-grey-1": "#dadce0",
+//         "brand-blue-1": "#1967d2",
+//         "brand-green-1": "#137333"
+
+//       },
+//     },
+//   },
+
+//   plugins: [],
+// };
+
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  content: ["./public/**/*.html", "./src.**.*.{vue,js,ts,jsx,tsx}"],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "brand-gray-1": "#dadce0",
+        "brand-blue-1": "#1967d2",
+        "brand-green-1": "#137333",
+      },
     },
   },
-
   plugins: [],
 };
