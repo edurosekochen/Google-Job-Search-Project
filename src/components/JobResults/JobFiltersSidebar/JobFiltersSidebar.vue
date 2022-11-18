4e5060643ff9c4d520b5580bfe2367d5caac7408
Eduardo Rosenberg <eduardo.rose92@gmail.com>
2022-11-18 19:47:09 -0300

    section-22 final

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -8,13 +8,37 @@
 			w-96
 		"
 	>
-		Job Filters Sidebar
+		<section class="pb-5">
+			<div class="flex flex-row justify-between">
+				<h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+				<div class="flex items-center text-sm">
+					<action-button text="Clear filters" type="secondary" />
+				</div>
+			</div>
+
+			<accordion header="Degree" />
+
+			<accordion header="Job Types">
+				<p>Placeholder for now</p>
+			</accordion>
+
+			<job-filters-sidebar-organizations />
+		</section>
 	</div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+import Accordion from "@/components/Shared/Accordion.vue";
+import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
+
 export default {
 	name: "JobFiltersSidebar",
+	components: {
+		ActionButton,
+		Accordion,
+		JobFiltersSidebarOrganizations,
+	},
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
 import { createApp } from "vue";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleDown,
+  faAngleUp,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 import App from "@/App.vue";
 import "@/assets/tailwind.css";
 import router from "@/router";
-import store from '@/store'
+import store from "@/store";
 
-
+library.add(faAngleDown);
+library.add(faAngleUp);
 library.add(faSearch);
 
-createApp(App).use(store)
+createApp(App)
+  .use(store)
   .use(router)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
-
-  
