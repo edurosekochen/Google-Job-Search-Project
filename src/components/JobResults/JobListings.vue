2e6a3384083e19c1d9cf4f5bde5d1849f094d140
Eduardo Rosenberg <eduardo.rose92@gmail.com>
2022-12-03 21:25:31 -0300

    section-29 final

--- src/components/JobResults/JobListings.vue
+++ src/components/JobResults/JobListings.vue
@@ -1,79 +1,75 @@
 <template>
-  <main class="flex-auto p-8 bg-brand-gray-2">
-    <ol>
-      <job-listing
-        v-for="job in displayedJobs"
-        :key="job.id"
-        :job="job"
-        data-test="job-listing"
-      />
-    </ol>
+	<main class="flex-auto p-8 bg-brand-gray-2">
+		<ol>
+			<job-listing
+				v-for="job in displayedJobs"
+				:key="job.id"
+				:job="job"
+				data-test="job-listing"
+			/>
+		</ol>
 
-    <div class="mt-8 mx-auto">
-      <div class="flex flex-row flex-nowrap">
-        <p class="text-sm flex-grow">Page {{ currentPage }}</p>
+		<div class="mt-8 mx-auto">
+			<div class="flex flex-row flex-nowrap">
+				<p class="text-sm flex-grow">Page {{ currentPage }}</p>
 
-        <div class="flex items-center justify-center">
-          <router-link
-            v-if="previousPage"
-            :to="{ name: 'JobResults', query: { page: previousPage } }"
-            class="mx-3 text-sm font-semibold text-brand-blue-1"
-            data-test="previous-page-link"
-            >Previous</router-link
-          >
+				<div class="flex items-center justify-center">
+					<router-link
+						v-if="previousPage"
+						:to="{ name: 'JobResults', query: { page: previousPage } }"
+						class="mx-3 text-sm font-semibold text-brand-blue-1"
+						data-test="previous-page-link"
+						>Previous</router-link
+					>
 
-          <router-link
-            v-if="nextPage"
-            :to="{ name: 'JobResults', query: { page: nextPage } }"
-            class="mx-3 text-sm font-semibold text-brand-blue-1"
-            data-test="next-page-link"
-            >Next</router-link
-          >
-        </div>
-      </div>
-    </div>
-  </main>
+					<router-link
+						v-if="nextPage"
+						:to="{ name: 'JobResults', query: { page: nextPage } }"
+						class="mx-3 text-sm font-semibold text-brand-blue-1"
+						data-test="next-page-link"
+						>Next</router-link
+					>
+				</div>
+			</div>
+		</div>
+	</main>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
+import { computed, onMounted } from "vue";
+import useCurrentPage from "@/composables/useCurrentPage";
+import usePreviousAndNextPages from "@/composables/usePreviousAndNextPages";
 
-import { FETCH_JOBS, FILTERED_JOBS } from "@/store/constants";
+import { useFilteredJobs, useFetchJobsDispatch } from "@/store/composables";
+
 import JobListing from "@/components/JobResults/JobListing.vue";
 
 export default {
-  name: "JobListings",
-  components: {
-    JobListing,
-  },
-  computed: {
-    ...mapGetters([FILTERED_JOBS]),
-    currentPage() {
-      const pageString = this.$route.query.page || "1";
-      return Number.parseInt(pageString);
-    },
-    previousPage() {
-      const previousPage = this.currentPage - 1;
-      const firstPage = 1;
-      return previousPage >= firstPage ? firstPage : undefined;
-    },
-    nextPage() {
-      const nextPage = this.currentPage + 1;
-      const maxPage = Math.ceil(this.FILTERED_JOBS.length / 10);
-      return nextPage <= maxPage ? nextPage : undefined;
-    },
-    displayedJobs() {
-      const pageNumber = this.currentPage;
-      const firstJobIndex = (pageNumber - 1) * 10;
-      const lastJobIndex = pageNumber * 10;
-      return this.FILTERED_JOBS.slice(firstJobIndex, lastJobIndex);
-    },
-  },
-  async mounted() {
-    this.FETCH_JOBS();
-  },
-  methods: {
-    ...mapActions([FETCH_JOBS]),
-  },
+	name: "JobListings",
+	components: {
+		JobListing,
+	},
+	setup() {
+		onMounted(useFetchJobsDispatch);
+
+		const filteredJobs = useFilteredJobs();
+		const currentPage = useCurrentPage();
+
+		const maxPage = computed(() => Math.ceil(filteredJobs.value.length / 10));
+
+		const { previousPage, nextPage } = usePreviousAndNextPages(
+			currentPage,
+			maxPage
+		);
+
+		const displayedJobs = computed(() => {
+			const pageNumber = currentPage.value;
+			const firstJobIndex = (pageNumber - 1) * 10;
+			const lastJobIndex = pageNumber * 10;
+			return filteredJobs.value.slice(firstJobIndex, lastJobIndex);
+		});
+
+		return { previousPage, nextPage, currentPage, displayedJobs };
+	},
 };
 </script>
--- tests/unit/store/composables.test.js
+++ tests/unit/store/composables.test.js
@@ -1,8 +1,12 @@
 import { useStore } from "vuex";
 jest.mock("vuex");
 
-import { useFilteredJobs, useUniqueJobTypes, useUniqueOrganizations } from "@/store/composables";
-import getters from "@/store/getters";
+import {
+  useFilteredJobs,
+  useUniqueJobTypes,
+  useUniqueOrganizations,
+  useFetchJobsDispatch,
+} from "@/store/composables";
 
 describe("composables", () => {
   describe("useFilteredJobs", () => {
@@ -16,29 +20,40 @@ describe("composables", () => {
     });
   });
   describe("useUniqueJobTypes", () => {
-    it('retrieves unique job types from store', () => {
-      useStore.mockReturnValue(
+    it("retrieves unique job types from store", () => {
+      useStore.mockReturnValue({
         getters: {
           UNIQUE_JOB_TYPES: new Set(["Full-time"]),
         },
-      )
+      });
 
       const result = useUniqueJobTypes();
       expect(result.value).toEqual(new Set(["Full-time"]));
-    })
-  })
+    });
+  });
 
   describe("useUniqueOrganizations", () => {
     it("retrieves unique job organizations from store", () => {
-      useStore.mockReturnValue(
+      useStore.mockReturnValue({
         getters: {
           UNIQUE_ORGANIZATIONS: new Set(["Apple"]),
         },
-      )
+      });
 
       const result = useUniqueOrganizations();
       expect(result.value).toEqual(new Set(["Apple"]));
-    })
-   
-  }) 
+    });
+  });
+
+  describe("useFetchJobsDispatch", () => {
+    it("sends call to fetch jobs from API", () => {
+      const dispatch = jest.fn();
+      useStore.mockReturnValue({
+        dispatch,
+      });
+      useFetchJobsDispatch();
+      expect(dispatch).toHaveBeenCalledWith("FETCH_JOBS");
+    });
+  });
+  
 });
